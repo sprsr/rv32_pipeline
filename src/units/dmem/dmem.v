cd5f78546fb92e21603849cc83c91513945f737f
sprsr <projecthourglass@protonmail.com>
2023-11-05 16:40:20 -0600

    testing

--- src/units/dmem/dmem.v
+++ src/units/dmem/dmem.v
@@ -1,19 +1,24 @@
-
 // Data Cache.  
 module dmem(
+    input         clk,
+    input         rst,
     input  [31:0] i_addr,
     input  [31:0]  dataW,
     input          memRW,
-    output [31:0] o_data
+    output wire [31:0] o_data
 );
 
+wire [31:0] w_data;
 reg [31:0] memory [0:1023];
+assign o_data = w_data;
 
-    always @(address) begin
-        if memRW begin
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            w_data[31:0] <= 32'd0;
+        end else if (memRW) begin
             memory[i_addr] <= dataW;
         end else begin
-            o_data <= memory[i_addr];
+            w_data <= memory[i_addr];
         end
     end    
 endmodule
