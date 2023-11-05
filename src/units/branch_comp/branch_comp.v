2fc5a7e3cabcac463b1ff5a92dad3015ab073168
sprsr <projecthourglass@protonmail.com>
2023-11-05 16:00:49 -0600

    branch compare compiles

--- src/units/branch_comp/branch_comp.v
+++ src/units/branch_comp/branch_comp.v
@@ -1,34 +1,39 @@
 module branch_comp(
-    input [31:0] i_dataA;
-    input [31:0] i_dataB;
-    input        brUn;
-    output       brEq;
-    output       brLT;
+    input [31:0] i_dataA,
+    input [31:0] i_dataB,
+    input        brUn,
+    output  wire brEq,
+    output  wire brLT
 );
 
+    reg r_Eq;
+    reg r_LT;
 
+    assign brEq = r_Eq;
+    assign brLT = r_LT;
+    
     always @(i_dataA or i_dataB) begin
         if (brUn) begin
             if (i_dataA == i_dataB) begin
-                brEq <= 1'b1;
-                brLT <= 1'b0;
-            end else if (i_dataA < i_datab) begin
-                brEq <= 1'b0;
-                brLT <= 1'b1;
+                r_Eq <= 1'b1;
+                r_LT <= 1'b0;
+            end else if (i_dataA < i_dataB) begin
+                r_Eq <= 1'b0;
+                r_LT <= 1'b1;
             end else begin
-                brEq <= 1'b0;
-                brLT <= 1'b0;
+                r_Eq <= 1'b0;
+                r_LT <= 1'b0;
             end
         end else begin
-            if ($signed(i_dataA) == $signed(i_datab))) begin
-                brEq <= 1'b1;
-                brLT <= 1'b0;
-            end else if ($signed(i_dataa) < $signed(i_datab)) begin
-                brEq <= 1'b0;
-                brLT <= 1'b1;
+            if (i_dataA == i_dataB) begin
+                r_Eq <= 1'b1;
+                r_LT <= 1'b0;
+            end else if (i_dataA < i_dataB) begin
+                r_Eq <= 1'b0;
+                r_LT <= 1'b1;
             end else begin
-                brEq <= 1'b0;
-                brLT <= 1'b0;
+                r_Eq <= 1'b0;
+                r_LT <= 1'b0;
             end    
         end
     end
