cc014d077bcac74c4769e04bb4d678cee4a674de
sprsr <projecthourglass@protonmail.com>
2023-11-05 21:59:31 -0600

    pc_sel compiles g2012

--- src/units/control/pc_sel/pc_sel.v
+++ src/units/control/pc_sel/pc_sel.v
@@ -1,14 +1,15 @@
 module PCSEL( input [6:0]   opcode,
-              output       pc_sel
+              output        pc_sel
 );
-
-    always @(opcode)
+reg r_pc_sel;
+assign pc_sel = r_pc_sel;
+    always @(*)
     begin
-            case(opcode)
-                7'h63: pc_sel   = 1'b1;
-                7'h67: pc_sel   = 1'b1;
-                7'h6f: pc_sel   = 1'b1;
-                default: pc_sel = 1'b0;
+            case (opcode)
+                7'h63: r_pc_sel <= 1'b1;
+                7'h67: r_pc_sel <= 1'b1;
+                7'h6f: r_pc_sel <= 1'b1;
+                default: r_pc_sel <= 1'b0;
             endcase
     end
 endmodule
