b5243ade02a817e96b595abb5df2df908d0a12af
sprsr <projecthourglass@protonmail.com>
2023-11-06 22:22:31 -0600

    LUI AUIPC JAL updated

--- src/units/control/AluSel/ALUSEL.v
+++ src/units/control/AluSel/ALUSEL.v
@@ -25,7 +25,9 @@ assign a_sel   = r_a_sel;
 assign b_sel   = r_b_sel;
 assign alu_sel = r_alu_sel;
 assign mem_wr  = r_mem_wr;
+assign RegWEn  = r_RegWEn;
 assign immSel  = r_immSel;
+assign BrUn    = r_BrUn;
 assign pc_sel  = r_pc_sel;
 assign wb_sel  = r_wb_sel;
 
@@ -37,7 +39,9 @@ always @(*) begin
             r_b_sel   <= 1'b1;
             r_alu_sel <= 4'b1001;
             r_mem_wr  <= 1'b0;
+            r_RegWEn  <= 1'b1;
             r_immSel  <= 4'h4;
+            r_BrUn    <= 1'bx;
             r_pc_sel  <= 1'b0;
             r_wb_sel  <= 2'b01;
         end
@@ -47,7 +51,9 @@ always @(*) begin
             r_b_sel   <= 1'b1;
             r_alu_sel <= 4'b0010;
             r_mem_wr  <= 1'b0;
+            r_RegWEn  <= 1'b1;
             r_immSel  <= 4'h4;
+            r_BrUn    <= 1'bx;
             r_pc_sel  <= 1'b0;
             r_wb_sel  <= 2'b01;
         end
@@ -58,8 +64,9 @@ always @(*) begin
             r_alu_sel <= 4'b0010;
             r_mem_wr  <= 1'b0;
             r_immSel  <= 4'h5;
-            r_pc_sel  <= 1'b0;
-            r_wb_sel  <= 2'b01;
+            r_BrUn    <= 1'bx; 
+            r_pc_sel  <= 1'b1;
+            r_wb_sel  <= 2'b10;
         end
             
     endcase
