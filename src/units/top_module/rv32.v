a5023197fa5f5aedb8bdee5488ef2fbcbd58c5c8
sprsr <projecthourglass@protonmail.com>
2023-11-11 08:46:16 -0600

    added PC to top mod

--- src/units/top_module/rv32.v
+++ src/units/top_module/rv32.v
@@ -1,3 +1,26 @@
 module rv32(
+    input clk,
+    input rst,
+    output zero
+);
 
+
+wire w_pc_sel;
+wire w_pc_4;
+wire w_o_alu;
+wire w_pc;
+
+control inst_control(
 )
+
+PC inst_pc(
+    .clk(clk),
+    .rst(rst),
+    .sel_pc(w_sel_pc),
+    .in_pc(w_pc_4),
+    .in_alu(w_o_alu),
+    .pc_nxt(w_pc_4),
+    .pc(w_pc)
+);
+
+
