61407fdbdb4a774f9510d835487da88518f6ab04
sprsr <projecthourglass@protonmail.com>
2023-11-12 01:06:58 -0600

    adding simple python script to turn assembly files into binaries

---
+++ test/assembler.py
@@ -0,0 +1,22 @@
+from riscv_assembler.convert import AssemblyConverter as AC
+# instantiate
+convert = AC(output_mode = 'f', nibble_mode = True, hex_mode = False)
+
+# Convert a whole .s file to text file
+#convert("simple.s" "result.txt")
+
+# Convert string of assembly to bin
+cnv_str = "add x1 0x 0x\nadd x2 x0 x1"
+convert(cnv_str, "result.bin")
+
+
+# Convert a string and print output with no nibbles
+convert.output_mode = 'p'
+convert.nibble_mode = False
+convert.convert(cnv_str)
+
+
+# Convert a string and save to array
+convert.output_mode = 'a'
+result = convert(cnv_str)
+
