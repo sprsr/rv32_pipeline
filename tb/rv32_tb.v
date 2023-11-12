72fb793acb051f921f1e7e49a6ca0781190fe807
sprsr <projecthourglass@protonmail.com>
2023-11-12 00:43:28 -0600

    testing

--- tb/rv32_tb.v
+++ tb/rv32_tb.v
@@ -2,16 +2,28 @@ module rv32_tb;
 
     //reset that pulses once
     reg reset = 0;
-    initial begin
-        # 17 reset = 1'b1;
-        # 11 reset = 1'b0;
-        # 29 rest  = 1'b1;
-        # 11 reset = 1'b0;
-        # 100 $stop;
+    reg clk;
+    
+    rv32 c0 (
+        .clk(clk),
+        .rst(reset)
+    );
+
+    initial begin 
+        clk = 1'b0;
+        forever #5 clk = ~clk;
     end
 
-    reg clk = 0;
-    always #5 clk = !clk;
+    initial begin
 
-    rv32 c0 (clk, reset);
+        $dumpfile("rv32_tb.vcd");
+        $dumpvars(0,rv32_tb);
+
+        # 17 reset = 1'b1;
+        # 11 reset = 1'b0;
+        # 29 reset = 1'b1;
+        # 11 reset = 1'b0;
+        # 100 $finish;
+    end
+
 endmodule
