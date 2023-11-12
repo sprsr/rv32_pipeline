efe7129e0802d1897bf3501bcfde137e0b3696fb
sprsr <projecthourglass@protonmail.com>
2023-11-12 11:10:15 -0600

    assembler environment

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -3,20 +3,20 @@ from riscv_assembler.convert import AssemblyConverter as AC
 convert = AC(output_mode = 'f', nibble_mode = True, hex_mode = False)
 
 # Convert a whole .s file to text file
-#convert("simple.s" "result.txt")
+convert("test.s" "test.out")
 
 # Convert string of assembly to bin
-cnv_str = "add x1 0x 0x\nadd x2 x0 x1"
-convert(cnv_str, "result.bin")
+#cnv_str = "add x1 0x 0x\nadd x2 x0 x1"
+#convert(cnv_str, "result.bin")
 
 
 # Convert a string and print output with no nibbles
-convert.output_mode = 'p'
-convert.nibble_mode = False
-convert.convert(cnv_str)
+#convert.output_mode = 'p'
+#convert.nibble_mode = False
+#convert.convert(cnv_str)
 
 
 # Convert a string and save to array
-convert.output_mode = 'a'
-result = convert(cnv_str)
+#convert.output_mode = 'a'
+#result = convert(cnv_str)
 
---
+++ assembler/test.s
@@ -0,0 +1,7 @@
+.section .text
+.align 2
+.global start
+
+start:
+    addi x11, x11, 1 #i ++
+    ret
