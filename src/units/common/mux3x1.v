5f40b73ca7567f09824b828b8f65c9a035539c8e
sprsr <projecthourglass@protonmail.com>
2023-11-11 18:51:08 -0600

    mux fix

--- src/units/common/mux3x1.v
+++ src/units/common/mux3x1.v
@@ -1,14 +1,16 @@
-module mux2x1(
+module mux3x1(
     input [31:0]      a,
     input [31:0]      b,
-    input             sel,
+    input [31:0]      c,
+    input [1:0]       sel,
     output reg [31:0] y
 );
 
     always @(*) begin
         case(sel)
-            1'b1 : y = a;
-            1'b0 : y = b;
+            2'h2 : y = a
+            2'h1 : y = b;
+            2'h0 : y = c;
         endcase    
     end
 endmodule
