f5b0aa571a16af87deb98160f7207a852b6043d2
sprsr <projecthourglass@protonmail.com>
2023-11-12 22:00:34 -0600

    reg test bench

--- tb/reg_tb/reg_tb.v
+++ tb/reg_tb/reg_tb.v
@@ -1,21 +1,26 @@
-module pc_tb;
+module reg_tb;
 
     //reset that pulses once
     reg reset = 0;
     reg clk;
-    reg sel_pc = 0;
-    reg [31:0] in_alu = 32'h00008000;
-    reg [31:0] in_pc_nxt;
-    reg [31:0] pc;
+    reg r_regWEn;
+    reg [31:0] r_dataD;
+    reg [4:0]  r_addrD;
+    reg [4:0]  r_addrA;
+    reg [4:0]  r_addrB;
+    reg [31:0] r_dataA;
+    reg [31:0] r_dataB;
     
-    PC c0 (
+    register reg_0 (
         .clk(clk),
         .rst(reset),
-        .sel_pc(sel_pc),
-        .in_pc(in_pc_nxt),
-        .in_alu(in_alu),
-        .pc_nxt(in_pc_nxt),
-        .pc(pc)
+        .regWEn(r_regWEn),
+        .dataD(r_dataD),
+        .addrD(r_addrD),
+        .addrA(r_addrA),
+        .addrB(r_addrB),
+        .dataA(r_dataA),
+        .dataB(r_dataB)
     );
 
     initial begin 
@@ -25,12 +30,36 @@ module pc_tb;
 
     initial begin
 
-        $dumpfile("pc_tb.vcd");
-        $dumpvars(0,pc_tb);
+        $dumpfile("reg_tb.vcd");
+        $dumpvars(0,reg_tb);
 
-        # 10 reset = 1'b1;
-        # 12 reset = 1'b0;
-        # 50 sel_pc = 1'b0; 
+        # 0  reset = 1'b1;
+        # 10 reset = 1'b0;
+
+        # 10 begin
+            r_addrA <= 5'd7;
+            r_addrB <= 5'd19;
+            r_regWEn<= 1'b0;
+        end
+        # 10 begin
+            r_addrA <= 5'd8;
+            r_addrB <= 5'd11;
+            r_regWEn<= 1'b0;
+        end
+        # 10 begin
+            r_dataD <= 32'h8;
+            r_addrD <= 5'd4;
+            r_addrA <= 5'd7;
+            r_addrB <= 5'd11;
+            r_regWEn<= 1'b1;
+        end
+        # 10 begin
+            r_dataD <= 32'h8;
+            r_addrD <= 5'd4;
+            r_addrA <= 5'd4;
+            r_addrB <= 5'd11;
+            r_regWEn<= 1'b0;
+        end
         # 200 $finish;
     end
 
