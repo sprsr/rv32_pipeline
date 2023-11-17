c6e87b1c9871f0d172db5903edd6d05c297eda25
sprsr <projecthourglass@protonmail.com>
2023-11-16 23:54:13 -0600

    exe continued moeved simple_instr_ctl

--- src/units/control/execute_ctl/execute_ctl.v
+++ src/units/control/execute_ctl/execute_ctl.v
@@ -1,6 +1,18 @@
 module execute_ctl(
-    input clk,
-    input rst,
-    input [31:0] instruction,
+    input         clk,
+    input         rst,
+    input         BrEq,
+    input         BrLT,
+    input  [31:0] instruction,
+    output        a_sel,
+    output        b_sel,
+    output        sign,
+    output [3:0]  alu_sel,
     output [31:0] instr_acc
 );
+
+reg       r_a_sel   = 1'b0;
+reg       r_b_sel   = 1'b0;
+reg [3:0] r_alu_sel = 4'b0;
+reg       r_sign    = 1'b0;
+
