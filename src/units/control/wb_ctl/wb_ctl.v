8152c95225bbe133fab489a44d29a6590184afe7
sprsr <projecthourglass@protonmail.com>
2023-11-19 23:12:42 -0600

    wb control pipelined

--- src/units/control/wb_ctl/wb_ctl.v
+++ src/units/control/wb_ctl/wb_ctl.v
@@ -1,18 +1,30 @@
 module wb_ctl(
     input clk,
     input rst,
+    input  [31:0] pc_4_acc,
+    input  [31:0] alu_out_acc,
+    input  [31:0] dmem_out,
     input  [31:0] instruction,
     output [1:0] wb_sel,
     output       regWEn,
+    output [31:0] pc_4_wb,
+    output [31:0] alu_out_wb,
+    output [31:0] dmem_out_wb,
     output [31:0] instr_wb
 );
 
 reg [1:0] r_wb_sel;
 reg       r_regWEn;
+reg [31:0] r_pc_4_wb;
+reg [31:0] r_alu_out_wb;
+reg [31:0] r_dmem_out_wb;
 reg [31:0] r_instr_wb;
 
 assign wb_sel = r_wb_sel;
 assign regWEn = r_regWEn;
+assign pc_4_wb = r_pc_4_wb;
+assign alu_out_wb = r_alu_out_wb;
+assign dmem_out_wb = r_dmem_out_wb;
 assign instr_wb = r_instr_wb;
 
 always @(posedge clk or posedge rst) begin
@@ -61,6 +73,9 @@ always @(posedge clk or posedge rst) begin
                 r_regWEn  <= 1'b0;
             end
         endcase
+        r_pc_4_wb<= pc_4_acc;
+        r_alu_out_wb <= alu_out_acc;
+        r_dmem_out_wb <= dmem_out;
         r_instr_wb <= instruction;
     end
 end
