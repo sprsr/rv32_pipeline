9c89408d0b28ae0d6d38ffff53ec9293efc3ab36
sprsr <projecthourglass@protonmail.com>
2023-11-20 23:26:45 -0600

    in progress instruction manager

--- src/units/instr_mgr/instr_mgr.v
+++ src/units/instr_mgr/instr_mgr.v
@@ -10,8 +10,10 @@ module instr_mgr(
     output[31:0] data_b_mgr
 );
 
+reg [3:0] r_conflict_map;
+
 //Function checking if the instruction is a write back instruction
-function [3:0] write_back_check;
+function [2:0] write_back_check;
     input [31:0] instruction;
     case (instruction[6:0])
         // LUI Instruction: 
@@ -47,15 +49,36 @@ function [3:0] write_back_check;
     endcase
 endfunction
 
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
-        end else begin
-            if ((instr_acc[11:7] == instr_de[19:15]) or (instr_acc[11:7] == instr_de[24:20])) begin
-                acc_wb = write_back_check(instr_acc);
-            end else if ((instr_exe[11:7] == instr_de[19:15]) or (instr_exe[11:7] == instr_de[24:20])) begin
-                acc_wb = write_back_check(instr_exe);
-            end
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        r_conflict_map = 4'b0;
+    end else begin
+        if (instr_acc[11:7] == instr_de[19:15]) begin
+            r_conflict_map[3] = 1'b1;
+        end else if (instr_acc[11:7] == instr_de[24:20]) begin
+            r_conflict_map[2] = 1'b1;
+        end else if (instr_exe[11:7] == instr_de[19:15]) begin
+            r_conflict_map[1] = 1'b1;
+        end else if(instr_exe[11:7] == instr_de[24:20]) begin
+            r_conflict_map[0] = 1'b1;
+        end
+        if (r_conflict_map[3]) begin
+            r_acc_wb = write_back_check(instr_acc);
+            case (r_acc_wb):
+                3'b00: begin
+                    r_data_out_a = dmem_out_acc;
+                end
+                3'b01: begin
+                    r_data_out_a = alu_out_acc;
+                end
+                3'b10: begin
+                    r_data_out_a = pc_4_acc;
+                end
+                    
+
+            acc_wb = write_back_check(instr_exe);
         end
     end
+end
 
-    endmodule
+endmodule
