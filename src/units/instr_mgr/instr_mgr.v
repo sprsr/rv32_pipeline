144bc4999448f1cf760601d194413a583bc2d0c7
sprsr <projecthourglass@protonmail.com>
2023-11-22 00:01:05 -0600

    added 2 hazards

--- src/units/instr_mgr/instr_mgr.v
+++ src/units/instr_mgr/instr_mgr.v
@@ -10,13 +10,15 @@ module instr_mgr(
     input [31:0] dmem_out_acc,
     input [31:0] pc_4_acc,
     output       stall,
-    output       hazard,
+    output       hazard_a,
+    output       hazard_b,
     output[31:0] data_a_mgr,
     output[31:0] data_b_mgr
 );
 
 reg       r_stall;
-reg       r_hazard;
+reg       r_hazard_a;
+reg       r_hazard_b;
 reg [3:0] r_conflict_map;
 reg [2:0] r_wb_acc;
 reg [2:0] r_wb_exe;
@@ -25,7 +27,8 @@ reg [31:0] r_data_a_mgr;
 reg [31:0] r_data_b_mgr;
 
 assign stall = r_stall;
-assign hazard = r_hazard;
+assign hazard_a = r_hazard_a;
+assign hazard_b = r_hazard_b;
 assign data_a_mgr = r_data_a_mgr;
 assign data_b_mgr = r_data_b_mgr;
 
@@ -70,7 +73,8 @@ always @(posedge clk or posedge rst) begin
     if (rst) begin
         r_conflict_map = 4'b0;
         r_stall = 1'b0;
-        r_hazard = 1'b0;
+        r_hazard_a = 1'b0;
+        r_hazard_b = 1'b0;
     end else begin
         if (instr_acc[11:7] == instr_de[19:15]) begin
             r_conflict_map[3] = 1'b1;
@@ -87,25 +91,23 @@ always @(posedge clk or posedge rst) begin
                 3'b00: begin
                     r_stall = 1'b1;
                     r_data_mgr = 32'hx;
-                    r_hazard = 1'b1;
                 end
                 3'b01: begin
                     r_data_mgr = alu_out_exe;
-                    r_hazard = 1'b1;
                 end
                 3'b10: begin
                     r_data_mgr = pc_exe + 1'b1;
-                    r_hazard = 1'b1;
                 end
                 default: begin
                     r_data_mgr = 32'hx;
-                    r_hazard = 1'b0;
                 end
             endcase
             if (r_conflict_map[1]) begin
                 r_data_a_mgr = r_data_mgr;
+                r_hazard_a = 1'b1;
             end else if (r_conflict_map[0]) begin
                 r_data_b_mgr = r_data_mgr;
+                r_hazard_b = 1'b1;
             end
         end 
         if (r_conflict_map[3] || r_conflict_map[2]) begin
@@ -113,25 +115,23 @@ always @(posedge clk or posedge rst) begin
             case (r_wb_acc)
                 3'b00: begin
                     r_data_mgr = dmem_out_acc;
-                    r_hazard = 1'b1;
                 end
                 3'b01: begin
                     r_data_mgr = alu_out_acc;
-                    r_hazard = 1'b1;
                 end
                 3'b10: begin
                     r_data_mgr = pc_4_acc;
-                    r_hazard = 1'b1;
                 end
                 default: begin
                     r_data_mgr = 32'hx;
-                    r_hazard = 1'b0;
                 end
             endcase
             if (r_conflict_map[3] && !r_conflict_map[1]) begin
                 r_data_a_mgr = r_data_mgr;
+                r_hazard_a = 1'b1;
             end else if (r_conflict_map[2] && !r_conflict_map[0]) begin
                 r_data_b_mgr = r_data_mgr;
+                r_hazard_b = 1'b1;
             end
         end
     end
