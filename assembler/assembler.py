365f381a968e915d4a663cbfd1d94dae0dab1fcc
sprsr <projecthourglass@protonmail.com>
2023-11-23 22:11:18 -0600

    removing whitespace

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -11,6 +11,13 @@ def convert_file(file):
     root, old_ext = os.path.splitext(file)
     output = f"{root}.txt"
     convert(file, output)
+
+    # Removing all whitespace for easy copying
+    with open(output, 'r') as file:
+        content = file.read()
+    new_content = re.sub(r'\s+', '', content)
+    with open(output, 'w') as file:
+        file.write(new_content)
     print(f'Generated Machine Code: {output}')
 
 def main():
@@ -19,6 +26,7 @@ def main():
         sys.exit(1)
     file_name = sys.argv[1]
     convert_file(file_name)
+
 
 if __name__ == "__main__":
     main()
